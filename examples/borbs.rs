738fd0f7e0a34a9b9ac1fcf3f2928fc1119ffe92
fncnt <git@vncnt.eu>
2023-02-14 15:58:07 +0100

    WIP toy example

--- examples/borbs.rs
+++ examples/borbs.rs
@@ -1,23 +1,20 @@
 use kiss3d::camera::ArcBall;
 use kiss3d::light::Light;
-use kiss3d::nalgebra::{distance, Point3, UnitVector3, Vector3};
+use kiss3d::nalgebra::{Point3, Vector3};
 use kiss3d::window::Window;
 use soa_derive::StructOfArray;
 use zelll::cellgrid::*;
 
-const NBORBS: usize = 10000;
-const SPEED: f64 = 0.25;
-const FOV: f64 = 1.7;
-const OUTER_RADIUS: f64 = 6.0;
-const ALIGNMENT: f64 = 0.85;
-//TODO: mixed up some signs, behaves more like COHESION
-const SEPARATION: f64 = 0.2;
-const STUBBORNESS: f64 = 0.4;
-const HOME: f64 = 0.04;
+const NBORBS: usize = 20000;
+const DELTA: f64 = 0.015;
+const OUTER_RADIUS: f64 = 1.0;
+const ALIGNMENT: f64 = 0.4;
+const SEPARATION: f64 = 0.4;
+const COHESION: f64 = 0.4;
+//const STUBBORNNESS: f64 = 1.0;
 
 //TODO: lessons from flamegraph:
-//TODO: nalgebra::Matrix::angle() is expensive
-//TODO: flattenign in point_pairs() is expensive due to allocating Vecs for nested iterators
+//TODO: flattening in point_pairs() is expensive due to allocating Vecs for nested iterators
 //TODO: par_point_pairs() in this naive implementation is inefficient
 
 fn main() {
@@ -42,68 +39,57 @@ fn main() {
     );
 
     let white = Point3::new(1.0, 1.0, 1.0);
+    let red = Point3::new(1.0, 0.0, 0.0);
 
     let mut cell_grid = CellGrid::new(&borbs.position, OUTER_RADIUS);
 
-    // average neighbor position (center of mass)
-    // with weighted contributions for separation and cohesion
-    let mut neighbor_com: Vec<Point3<f64>> = vec![Point3::default(); NBORBS];
-    // neighbor direction sum (i.e. not normalized)
-    let mut neighbor_dir: Vec<Vector3<f64>> = borbs
-        .direction
-        .iter()
-        .map(|uvec| (*uvec).into_inner())
-        .collect();
+    //TODO: scale by number of neighbors for each boid
+    let mut cohesion: Vec<Point3<f64>> = vec![Point3::default(); NBORBS];
+    let mut separation: Vec<Vector3<f64>> = vec![Vector3::default(); NBORBS];
+    let mut alignment: Vec<Vector3<f64>> = vec![Vector3::default(); NBORBS];
+    let mut neighborhood: Vec<usize> = vec![0; NBORBS];
 
     while window.render_with_camera(&mut cam) {
-        //TODO: this is definetly not correct yet
-        //TODO: and it's ugly
         cell_grid.point_pairs().for_each(|(borb, other)| {
-            let dist = distance(&borbs.position[borb], &borbs.position[other]);
-            //TODO: angle() is surprisingly expensive
-            let is_behind =
-                borbs.direction[borb].angle(&(borbs.position[borb] - borbs.position[other])) > FOV;
-            if dist <= OUTER_RADIUS && !is_behind {
-                if dist < OUTER_RADIUS * SEPARATION {
-                    neighbor_com[borb] -= borbs.position[other].coords;
-                } else {
-                    neighbor_com[borb] += borbs.position[other].coords;
-                }
-                neighbor_dir[borb] += borbs.direction[other].into_inner();
+            let relative_pos = borbs.position[borb] - borbs.position[other];
+
+            if relative_pos.norm() <= OUTER_RADIUS
+            //&& borbs.direction[borb].angle(&relative_pos) < 1.7
+            {
+                neighborhood[borb] += 1;
+
+                separation[borb] += relative_pos;
+                cohesion[borb] += borbs.position[other].coords;
+                alignment[borb] += borbs.direction[other];
             }
         });
         for (i, borb) in borbs.iter_mut().enumerate() {
             //TODO: proper error handling
-            let cohesion_separation = (neighbor_com[i] - *borb.position)
-                .try_normalize(f64::EPSILON)
-                .unwrap_or(Vector3::default());
-            let alignment = neighbor_dir[i]
-                .try_normalize(f64::EPSILON)
-                .unwrap_or(**borb.direction);
+            let borb_cohesion = cohesion[i] / neighborhood[i].max(1) as f64 - *borb.position;
+            let borb_separation = separation[i];
+            let borb_alignment = alignment[i] / neighborhood[i].max(1) as f64;
 
-            let home = borb
-                .position
-                .coords
-                .try_normalize(f64::EPSILON)
-                .unwrap_or(Vector3::default());
-
-            *borb.direction = UnitVector3::new_normalize(*borb.direction.slerp(
-                &UnitVector3::new_normalize(
-                    (1.0 - ALIGNMENT) * cohesion_separation + ALIGNMENT * alignment - HOME * home,
-                ),
-                1.0 - STUBBORNESS,
-            ));
+            *borb.direction += COHESION * borb_cohesion
+                + SEPARATION * borb_separation
+                + ALIGNMENT * borb_alignment;
         }
 
         for mut borb in borbs.iter_mut() {
-            borb.fly(SPEED);
+            borb.fly(DELTA);
             window.draw_point(&borb.position.cast::<f32>(), &white);
+            /*window.draw_line(
+                &borb.position.cast::<f32>(),
+                &(borb.position.cast::<f32>() + borb.direction.normalize().cast::<f32>()),
+                &red,
+            );*/
         }
 
         cell_grid = cell_grid.rebuild_mut(&borbs.position, None);
 
-        neighbor_dir.fill_with(Default::default);
-        neighbor_com.fill_with(Default::default);
+        cohesion.fill_with(Default::default);
+        separation.fill_with(Default::default);
+        alignment.fill_with(Default::default);
+        neighborhood.fill_with(Default::default);
     }
 }
 
@@ -111,22 +97,25 @@ fn main() {
 #[soa_derive(Debug)]
 pub struct Borb {
     position: Point3<f64>,
-    direction: UnitVector3<f64>,
+    direction: Vector3<f64>,
 }
 
 impl Borb {
     fn new_random() -> Self {
         Self {
             position: ((Vector3::new_random() - Vector3::new(0.5, 0.5, 0.5)) * 100.0).into(),
-            direction: UnitVector3::new_normalize(Vector3::new_random()),
+            direction: Vector3::new_random(),
         }
     }
 }
 
 impl BorbRefMut<'_> {
-    fn fly(&mut self, speed: f64) {
-        self.direction.renormalize_fast();
-        *self.position += (*self.direction).into_inner() * speed;
+    fn fly(&mut self, delta: f64) {
+        //self.direction.renormalize_fast();
+        let magnitude = self.direction.norm();
+        self.direction.normalize_mut();
+        *self.direction *= magnitude.min(10.0);
+        *self.position += *self.direction * delta;
     }
 }
 
